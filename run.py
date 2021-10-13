21d1023af07101f2c6c7e0f7a6cf6eab1932b882
Yasi <79868623+Yasi92@users.noreply.github.com>
2021-10-13 12:15:25 +0200

    add user request loop to repeat request for data until data provided is valid.

--- run.py
+++ run.py
@@ -22,17 +22,29 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print("Please enter the sales data from the last market")
-    print("Data should be six numbers, seperated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here:")
-    # the nex print statement is only to check 
-    # print(f"The data provided is {data_str}")
+    """
+    We wrap the func in a while loop so that everytime the data
+    is not valid it repeats the loop without breaking the program.
+    If the data is valid from validate_data func, the loop will break.
+    """
+    while True:
+        print("Please enter the sales data from the last market")
+        print("Data should be six numbers, seperated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    # The split method returns the broken up values as a list
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here:")
+        # the nex print statement is only to check 
+        # print(f"The data provided is {data_str}")
+
+        # The split method returns the broken up values as a list
+        sales_data = data_str.split(",")
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data        
+
 
 
 def validate_data(values):
@@ -49,6 +61,9 @@ def validate_data(values):
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")    
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
 
-get_sales_data()
+    return True        
+
+data = get_sales_data()
